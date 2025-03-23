151bf072429b325b9e633a7e40be0996f4d1daa2
cherukurir <rcherukuri@msn.com>
2025-03-23 16:27:30 -0700

    Working encoder and motor controller.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -8,28 +8,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class MotorTest extends LinearOpMode {
     private DcMotor Mot1;
 
-
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         Mot1 = hardwareMap.get(DcMotor.class, "motor");
-
         Mot1.setDirection(DcMotor.Direction.FORWARD);
-
         Mot1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
-
-
         while (opModeIsActive()) {
             double power = gamepad1.left_stick_y;
             Mot1.setPower(power);
-
-
             telemetry.addData("Motor 1", "power (%.2f)", Mot1.getPower());
-            telemetry.addData("Encoder Pos", "power (%.2f)", Mot1.getCurrentPosition());
+            telemetry.addData("Encoder Pos: ", Mot1.getCurrentPosition());
             telemetry.update();
         }
     }
